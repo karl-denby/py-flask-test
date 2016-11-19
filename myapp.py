015a4b5e98154d3b57a53eae190cc9c306cf65be
karl-denby <karl.denby@gmail.com>
2016-11-19 14:51:00 +0000

    Moment for time/locale

--- myapp.py
+++ myapp.py
@@ -1,12 +1,16 @@
 from sys import version
 from flask import Flask, redirect, request, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.errorhandler(404)
 def page_not_found(e):
