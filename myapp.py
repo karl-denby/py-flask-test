a480d25d6c9ebeffaf9c686c7028b54ae2091ca5
karl-denby <karl.denby@gmail.com>
2016-11-20 18:57:57 +0000

    working form example

--- myapp.py
+++ myapp.py
@@ -3,8 +3,16 @@ from flask import Flask, redirect, request, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'MySecretKeyForCSFR'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
@@ -21,10 +29,22 @@ def headers():
     user_agent = request.headers.get('User-Agent')
     return render_template('headers.html', user_agent=user_agent)
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+@app.route('/user/', methods=['GET', 'POST'])
+@app.route('/user/<name>', methods=['GET', 'POST'])
+def user(name=None):
+    test_form = NameForm()
+    if test_form.validate_on_submit():
+        name = test_form.name.data
+        test_form.name.data = ''
+        
+    if name == None:
+        name = "Anonymous"
+        
+    return render_template('user.html', name=name, form=test_form)
 
+
+#
 # Run the application
+#
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
