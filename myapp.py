17cdfbd7077dc483dda61652cdbd1ad49b98051b
karl-denby <karl.denby@gmail.com>
2016-11-24 11:30:25 +0000

    can send emails

--- myapp.py
+++ myapp.py
@@ -15,24 +15,40 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail, Message
+from threading import Thread
 
+
+#
+# Extentions
+#
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 manager = Manager(app)
-
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
+mail = Mail(app)
 
+
+#
+# Configuration
+#
 app.config['SECRET_KEY'] = 'MySecretKeyForCSFR'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 manager.add_command('db', MigrateCommand)
 manager.add_command('shell', Shell(make_context=make_shell_context))
+
+app.config['MAIL_SERVER'] = 'smtp.google.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_ADMIN'] = os.environ.get('MAIL_ADMIN')
 
 
 #
@@ -62,6 +78,22 @@ class User(db.Model):
 
 
 #
+# Functions
+#
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(subject, sender='noreply@myserver.com', recipients=to)
+    msg.body = render_template( template + '.txt', **kwargs)
+    msg.html = render_template( template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
+
+
+#
 # routes/controllers
 #
 @app.route('/')
@@ -86,6 +118,7 @@ def user(name=None):
         if db_user is None:
             db_user = User(username=form.name.data)
             db.session.add(db_user)
+            send_email(app.config['MAIL_ADMIN'], 'New User', 'mail/new_user', user=db_user)
             session['known'] = False
         else:
             session['known'] = True
